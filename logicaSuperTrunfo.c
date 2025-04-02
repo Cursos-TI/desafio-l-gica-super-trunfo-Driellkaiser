805184356cf964bd10a91eec4e88735b067a89ac
Driellkaiser <83596179+Driellkaiser@users.noreply.github.com>
2025-04-02 04:33:04 -0300

    Update logicaSuperTrunfo.c
    Projeto em andamento

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -41,3 +41,60 @@ int main() {
 
     return 0;
 }
+
+#include <studio.h>
+//Definição da estrutura para armazenar os dados da carta
+typedef struct {
+char estado;
+char codigo[4];
+char nome_cidade[50];
+int populacao;
+float area;
+float pib;
+int pontos_turisticos;
+} Carta;
+
+//Função para cadastrar os dados de uma carta
+void cadastrarCarta(Carta *carta, int numero) {
+    printf("\nCadastro da Carta %d:\n", numero);
+    printf("Estado (A-H):");
+    scanf("%c", carta->estado);
+    printf("Código da Carta (ex: A01):");
+    scanf("%3s", carta->codigo);
+    printf("Nome da Cidade");
+    scanf("%[^"]s", carta->nome_cidade);
+    printf("População:")
+    scanf("%d", carta->populacao);
+    printf("Área (em km):");
+    scanf("%f", carta->aera);
+    printf("PIB (em bilhões de reais):");
+    scanf("%f", carta->pib);
+    printf("Numero de Pontos Turísticos:");
+    scanf("%d", carta->pontos_turisticos);
+}
+
+//Funcção para exibir os dados de uma carta 
+void exibirCarta(Carta carta, int numero) {
+    printf("\nCarta: %d\n",numero);
+    printf("Estado: %c\n",carta.estado);
+    printf("Código: %s\n", carta.codigo);
+    printf("Nome da cidade: %s\n",carta.nome_cidade);
+    printf("População: %d\n",carta.populacao);
+    printf("Áera: %.2f km\n",carta.aera);
+    printf("PIB: %.2f bilhões de reais\n", carta.pib);
+    printf("Número de Pontos Turísticos: %d\n",carta.pontos_turisticos);
+}
+
+int man(){
+    //Cadastro das cartas 
+    cadastrarCarta(carta&1,1);
+    cadastrarCarta(carta&2,2);
+
+//Exibição dos dados cadastrados
+    exibirCarta(carta&1,1);
+    exibirCarta(carta&2,2);
+
+// 
+
+return 0;
+}
